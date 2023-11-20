4169402dfd18e19423f2b41d39eecf3d33248850
JunHyeok Seo <sjhgd107@naver.com>
2023-11-20 11:42:18 +0900

    first commit

---
+++ src/test/java/com/weatherlook/ootd/OotdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherlook.ootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
