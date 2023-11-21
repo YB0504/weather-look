f7ab2b8fab46b3e68514afce4f7cfbd5807217f5
CHR3936 <gpfus39362gmial.com>
2023-11-21 12:08:27 +0900

    community 브랜치에서 첫번째 커밋

--- bin/src/main/webapp/index.jsp
+++ bin/src/main/webapp/index.jsp
@@ -9,7 +9,8 @@
 <body>
 
 <script>
-    location.href="main2"
+
+location.href="main2"
 </script>
 
 </body>
---
+++ bin/src/main/webapp/sql/member.sql
@@ -0,0 +1,3 @@
+select * from member;
+
+insert into member values('선홍', '선홍', '1234', null, '우리집', '핸드폰', null, null);
