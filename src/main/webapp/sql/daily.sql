22181633b1ac69aedca39ba37e9a6a4084b3ae77
sun4red <klzxcv1@gmail.com>
2023-11-21 12:06:37 +0900

    test

--- src/main/webapp/sql/daily.sql
+++ src/main/webapp/sql/daily.sql
@@ -1,4 +1,7 @@
 select * from DAILY;
 
-insert into daily (post_id, title, read_count, reg_date, content, temperature, latitude, longitude, type_id, nick)
-values(1, '선홍의 글 제목', 0, sysdate, '글내용은 스킵', 10.5, 36.2, 128.4, 1, '선홍');
+insert into daily
+values(daily_seq.nextval, '데일리', 0, sysdate, '글내용은 스킵', 4.0, 37.3, 125.6, 1, '선홍');
+
+
+delete from daily where nick = '선홍';
